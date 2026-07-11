31a76985a9ec8c59ba36935260521a1626c75f36
Aryan Lalit Bijole <aryanbijole001@gmail.com>
2026-07-11 13:05:39 +0530

    email setting is changed

--- pm_platform/settings.py
+++ pm_platform/settings.py
@@ -160,8 +160,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'hizensoftsoftware@gmail.com'
-EMAIL_HOST_PASSWORD = 'ybimpdxftnnrbvlf'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
